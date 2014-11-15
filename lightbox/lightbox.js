7f93de5bd253d8169e4cc28d04b80f0c5ec130ea
Rui Bian <rui.bian@rea-group.com>
2014-11-15 13:18:06 +0800

    refine the resources.

--- lightbox/lightbox.js
+++ lightbox/lightbox.js
@@ -1,18 +1,19 @@
 $(function(){
-	$(".photo").click(openLightbox);
-	$(".overlay").click(closeLightbox);
-	$(".overlay img").click(stay);
-})
+    $(".photo").click(openLightbox);
+    $(".overlay").click(closeLightbox);
+    $(".overlay img").click(stay);
+});
 
 function openLightbox(){
-	var src = $(this).find("img").attr("src");
-	$(".overlay").fadeIn().find("img").attr("src", src);
+    $(".overlay").fadeIn();
+    var src = $(this).find("img").attr("src");
+    $(".overlay img").attr("src", src);
 }
 
 function closeLightbox(){
-	$(this).fadeOut();
+    $(this).fadeOut();
 }
 
 function stay(){
-	return false;
+    return false;
 }
